feacf1230b1e48d20a59c3e494d11e19b2b4e5a8
JorViteri <rorschacht14@gmail.com>
2023-06-10 20:27:22 +0200

    Codigo para consultas con BD SQL

---
+++ mysql.r
@@ -0,0 +1,29 @@
+library(RMySQL)
+library(dplyr)
+library(data.table)
+
+setwd('$$$$$') #PONER AQUI EL DIRECTORIO CORRESPONDIENTE
+#Se construye el objeto driver
+driver <- MySQL()
+
+#Parametros conexión
+host <- "127.0.0.1"
+port <- 3306
+usr<-"$$$$" #PONER EL USUARIO
+pass<-"$$$$" #PONER LA CONTRASEÑA DE CADA UNO 
+database <- "$$$$" #PONER EL NOMBRE DE LA BD
+
+#Se incia la conexion
+mydb <- dbConnect(driver, user = usr, password = pass,
+                  dbname=database, host=host,port=port)
+
+#Cargamos los datos
+routes_df <- fread("$$$$") #PONER AQUI EL DIRECTORIO CORRESPONDIENTE
+
+#Para que se puedan meter datos en el SQL hay que ejecutar en la:
+#SET GLOBAL local_infile=1;
+#Crea tabla y mete datos 
+dbWriteTable(mydb, "Routes", routes_df, append = TRUE, row.names = FALSE)
+
+#Cerrar conecxion
+dbDisconnect(mydb)
