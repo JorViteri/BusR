8bee3249b3c18db7a4e44c9109e5582e57f0b57f
JorViteri <rorschacht14@gmail.com>
2023-06-15 21:00:47 +0200

    new scripts

---
+++ fares.R
@@ -0,0 +1,164 @@
+library(httr)
+library(jsonlite)
+library(dplyr)
+library(stringr)
+library(data.table)
+library(stringi)
+
+#Con este scipt se cargan los datos del csv de las tarifas y se asocian a los ids de concellos.
+setwd('D:\\IFFE\\TFM\\csv\\') 
+
+#Se cargan los df necesarios 
+fares_df <- fread("fares.csv")
+municipalities_df <- fread("concellos.csv")
+
+#Se ponen en mayusculas los textos del df de tarifas
+fares_df$AYUNTAMIENTO_ORIGEN <- toupper(fares_df$AYUNTAMIENTO_ORIGEN)
+fares_df$AYUNTAMIENTO_DESTINO <- toupper(fares_df$AYUNTAMIENTO_DESTINO)
+
+
+
+#Se eliminan las tildes de los textos del df de tarifas
+fares_df$AYUNTAMIENTO_ORIGEN <- stri_replace_all_regex(fares_df$AYUNTAMIENTO_ORIGEN,
+                                                       pattern=c('á','é','í','ó','ú','Á','É','Í','Ó','Ú'),
+                                                       replacement=c('a','e','i','o','u','A','E','I','O','U'),
+                                                       vectorize=FALSE)
+
+fares_df$AYUNTAMIENTO_DESTINO <- stri_replace_all_regex(fares_df$AYUNTAMIENTO_DESTINO,
+                                                        pattern=c('á','é','í','ó','ú','Á','É','Í','Ó','Ú'),
+                                                        replacement=c('a','e','i','o','u','A','E','I','O','U'),
+                                                        vectorize=FALSE)
+
+
+#Ahora toca hacer el merge en base a substr
+#Primero obtenemos los unicos de origen y destino
+origin_df = data.frame(municipality=unique(fares_df$AYUNTAMIENTO_ORIGEN))
+destination_df = data.frame(municipality=unique(fares_df$AYUNTAMIENTO_DESTINO))
+
+#y comprobamos que sean identicos
+identical(origin_df,destination_df)
+#Son identicos, por lo que con uno nos deberia llegar
+
+#Aqui tenemos los que coinciden en texto con nuestro df
+equals_df <- merge(origin_df,municipalities_df,by="municipality")
+
+#y ahora obtenemos los concellos que no coinciden
+missing_df<-anti_join(origin_df,municipalities_df,by="municipality")
+
+
+
+
+#En el municipalidades hay que quitar tildes
+municipalities_df$municipality <- stri_replace_all_regex(municipalities_df$municipality,
+                                                         pattern=c('á','é','í','ó','ú','Á','É','Í','Ó','Ú'),
+                                                         replacement=c('a','e','i','o','u','A','E','I','O','U'),
+                                                         vectorize=FALSE)
+#En el missing poner guiones bajos en lugar de altos
+missing_df$municipality<- stri_replace_all_regex(missing_df$municipality,
+                                                 pattern=c('-'),
+                                                 replacement=c('_'),
+                                                 vectorize=FALSE)
+
+#Cambiar "SAN_CRISTOVO_DE_CEA" a "SAN_CRISTOBO_DE_CEA" en el missing_df
+missing_df$municipality[missing_df$municipality == "SAN_CRISTOVO_DE_CEA"] <-"SAN_CRISTOBO_DE_CEA"
+
+#Ahora generamos el df para el bucle
+last_df <- data.frame()
+#Por cada concello que falta
+for(i in 1:nrow(missing_df)){
+  #Iteramos los que ya tenemos
+  for(j in 1:nrow(municipalities_df)){
+    #Miramos si el que teniamos es sub cadena del uno y viceversa
+    comp1<- grepl(municipalities_df$municipality[j],missing_df$municipality[i], fixed=TRUE)
+    comp2<-grepl(missing_df$municipality[i],municipalities_df$municipality[j], fixed=TRUE)
+    
+    #Si se cumple cualquiera de los casos
+    if(comp1 || comp2){
+      #Obtenemos el id del concello
+      id <-c(municipalities_df$id[j])
+      #Luego el nombre
+      municipality <- c(missing_df$municipality[i])
+      #Y se añade la entrada al df resultante
+      last_df<-bind_rows(last_df,data.frame(id,municipality))
+      
+    }
+  }
+}
+
+#Con last_df y equals_df puedo filtar aquellos ids que ya tenia en equals_df, quedandome solo con los nuevos
+last_df<-anti_join(last_df,equals_df,by="id")
+
+#Ahora ya los podemos concatenar
+fares_mun_df <- unique(bind_rows(equals_df,last_df)) %>% na.omit()
+
+#preparamos el origin_df para hacer comparaciones cambianso los guiones altos por bajos
+origin_df$municipality<- stri_replace_all_regex(origin_df$municipality,
+                                                pattern=c('-'),
+                                                replacement=c('_'),
+                                                vectorize=FALSE)
+#Y modifcanto del concello previo
+origin_df$municipality[origin_df$municipality == "SAN_CRISTOVO_DE_CEA"] <-"SAN_CRISTOBO_DE_CEA"
+
+error <- anti_join(origin_df,fares_mun_df,by="municipality")
+#Hay 0 entradas
+
+error <- anti_join(fares_mun_df,origin_df,by="municipality")
+#Hay 0 entradas, está todo correcto
+
+#Se perada el df para su merge, cambiando el nombre de la columna
+fares_mun_df<- fares_mun_df %>%
+  rename(AYUNTAMIENTO_ORIGEN=municipality)
+
+#Hacemos el join en base al nombre de concello con el df de las tarifas
+fares_df1<- fares_df %>%
+  merge(fares_mun_df,by="AYUNTAMIENTO_ORIGEN")
+
+#Lo mismo, pero preparando la union en base a los destinos
+fares_mun_df<- fares_mun_df %>%
+  rename(AYUNTAMIENTO_DESTINO=AYUNTAMIENTO_ORIGEN)
+
+#Se cambian los nombres para diferenciar los ids
+fares_df2<- fares_df1 %>%
+  rename(id_origin=id)
+
+#Se hace la union, se modifica el nombre del nuevo id y se eliminan los columnas
+#de los nombres, no nos son necesarias
+fares_df2<- fares_df2 %>% 
+  merge(fares_mun_df,by="AYUNTAMIENTO_DESTINO") %>%
+  rename(id_destination=id) %>%
+  select(-c("AYUNTAMIENTO_ORIGEN","AYUNTAMIENTO_DESTINO"))
+
+#Escribimos el nuevo df en un csv
+write.csv(fares_df2, "D:\\IFFE\\TFM\\csv\\fares_df.csv",
+          row.names=FALSE,fileEncoding = "UTF-8")
+
+#Debido a la complejidad de los nombres de las columnas se han cambiando directamente en un editor de texto
+#Vamos a cargar para cambiar las columnas de lugar y añadir ids
+new_fares_df <- fread("D:\\IFFE\\TFM\\csv\\fares_new_names.csv")
+
+#Se acopla la columna de ids de fila y se reorganizan las columnas
+new_fares_df <- new_fares_df %>%
+  mutate(id = row_number()) %>%
+  select(c("id","id_origin","id_destination","journey","bonus"
+           ,"fee_in_cash","fee_tmg_until40",
+           "fee_tmg_until40with_discount",
+           "fee_tmg_more40","fee_tmg_more40with_discount"))
+
+
+#Convertir las columnas de precios en numericas
+new_fares_df$fee_in_cash <- as.numeric(gsub(",",".",gsub("€", "",new_fares_df$fee_in_cash )))
+new_fares_df$fee_tmg_until40 <- as.numeric(gsub(",",".",gsub("€", "",new_fares_df$fee_tmg_until40)))
+new_fares_df$fee_tmg_until40with_discount <- as.numeric(gsub(",",".",gsub("€", "",new_fares_df$fee_tmg_until40with_discount)))
+new_fares_df$fee_tmg_more40 <- as.numeric(gsub(",",".",gsub("€", "",new_fares_df$fee_tmg_more40)))
+new_fares_df$fee_tmg_more40with_discount <- as.numeric(gsub(",",".",gsub("€", "",new_fares_df$fee_tmg_more40with_discount)))                              
+
+#Hay una entrada Vigo-Vigo que estaba sin valor y queda a NAN-> se pone igual que a A Coruña
+new_fares_df$fee_in_cash [is.na(new_fares_df$fee_in_cash)] <- 1.55
+new_fares_df$fee_tmg_until40 [is.na(new_fares_df$fee_tmg_until40)] <- 0.88
+new_fares_df$fee_tmg_until40with_discount [is.na(new_fares_df$fee_tmg_until40with_discount)] <- 0.44
+new_fares_df$fee_tmg_more40 [is.na(new_fares_df$fee_tmg_more40)] <- 0.76
+new_fares_df$fee_tmg_more40with_discount [is.na(new_fares_df$fee_tmg_more40with_discount)] <- 0.38
+
+#Se escribe el csv definitivo, con los valores de € ya numericos
+write.csv(new_fares_df, "D:\\IFFE\\TFM\\csv\\fares_final.csv",
+          row.names=FALSE,fileEncoding = "UTF-8")
---
+++ missing_stops.R
@@ -0,0 +1,207 @@
+
+library(httr)
+library(jsonlite)
+library(dplyr)
+library(stringr)
+library(data.table)
+
+#Con este script se obtienen paradas que estaban presentes en los servicios pero no en nuestro df de éstas.
+
+#Constantes
+BASE_URL='https://tpgal-ws-externos.xunta.gal/tpgal_ws/rest/'
+STOPS_URL='busstops/autocomplete?text=%s&num_results=1000000'
+
+
+convertir <- function(datos){
+  # Esta funcion ayurdará a convertir info del API en dataframes
+  data = rawToChar(datos$content)
+  Encoding(data) <- 'UTF-8'
+  return (fromJSON(enc2utf8(data)))
+  
+}
+
+
+#Funcion con la que se obtiene el dataset de paradas de buses asocidas a su concello
+get_busstops_mun<-function(data){
+  #Inicializamos un dataframe vacío
+  busstops_df<-data.frame()
+  #Y una lista para los casos de error
+  errors <- list(c("0-0"=TRUE))
+  #Se crea un df con los valores unicos de inciales
+  initials <- data.frame(municipality=unique(data$municipality))
+  #Para cada posible inicial
+  for(i in 1:nrow(initials)) 
+  {
+    #Se construye la url de la petición
+    mun=initials$municipality[i]
+    url = sprintf(paste(BASE_URL,STOPS_URL,sep=''),paste(mun,'_',sep=''))
+    #Se ejecuta la petición
+    answer <- GET(url)
+    #Se comprueba que el mensaje tenga formato json
+    if((validate(rawToChar(answer$content)))==FALSE){
+      #En caso de error se almacena en "errors...
+      errors[[sprintf('%s',mun)]]=TRUE
+      #Y se avanza a la siguiente iteracion en j
+      next
+    }
+
+    answer <- convertir(answer)
+    #Se obtiene el df de los resultados
+    answer_df <- answer$results 
+    #Se comprueba que se tengan resultados
+    if(length(answer_df)>0){
+      
+      #Cogemos las ids correspondientes a las inciales
+      tmp_id<- data %>%
+        filter(mun==data$municipality) %>%
+        select("id")
+      #Genramos un df para el conjunto de paradas que se van a sacar
+      complete_df <- data.frame()
+      #Iteramos para los ids de los concellos
+      for(j in 1:nrow(tmp_id)){
+        
+        tmp_df <- answer_df %>%
+          #Filtramos las entradas de tipo 'municipality'...
+          filter(type!='municipality') %>%
+          #... y aquellas cuyo ID se corresponda al del concello
+          filter(startsWith(as.character(id), as.character(tmp_id$id[j])))
+        #Si hay filas...
+        if(nrow(tmp_df)>0){
+          #Asociamos el id del municipio a la parada
+          tmp_df$id_municipality=tmp_id$id[j]
+          #Y se almacena en el df
+          complete_df <- rbind(complete_df,tmp_df)
+        }
+      }
+      
+      #Si el df contiene paradas
+      if(nrow(complete_df)>0){
+        #Se renombran algunas columnas
+        complete_df <- complete_df %>% rename("stop_name"="text","id_stop"="id_sitme")
+        #Se concatena el df a busstops_df, que contendrá todas
+        busstops_df<-bind_rows(busstops_df,complete_df)
+      }
+    } 
+  }
+  return(busstops_df)
+}
+
+setwd('D:\\IFFE\\TFM\\csv\\') 
+
+#Cargamos los datos
+busstops_df <- fread("busstops.csv")
+services_df <- fread("services_new_id.csv")
+municipalities_df <- fread("concellos.csv")
+
+#Sacamos las paradas de origen a un df
+services_origins_df <- services_df %>% 
+                       select(id_origin) %>% 
+                       rename(id=id_origin) %>%
+                       distinct()
+
+#Lo mismo pero con las de destino
+services_destinations_df <- services_df %>% 
+                             select(id_destination) %>% 
+                             rename(id=id_destination) %>%
+                             distinct() 
+#Y las concatenamos para tener un df de las paradas de los servicios
+services_stops_id_df <- rbind(services_origins_df,services_destinations_df ) %>%
+                        distinct()
+
+#Sacamos los ids de las paradas que tenemos a un propio df, para mejor manejo
+busstops_id_df <- busstops_df %>% select(id)
+
+#Se hace el anti_join para obtener las paradas de los servicios que nos faltan
+missing_stops <- anti_join(services_stops_id_df,busstops_id_df)
+
+#Se transforman los ids a char y se cambia el nombre de la columna
+missing_stops <- data.frame(as.character(missing_stops$id)) %>%
+                                rename(id=as.character.missing_stops.id.)
+
+#En este bucle nos vamos a quedar con los primeros 5 caracteres de cada entrada id
+missing_municipalities_df<-data.frame()
+for(i in 1:nrow(missing_stops)){
+  #Se aplica la función con la que se extrae la parte de identificacion del concello
+  tmp <- str_extract(missing_stops$id[i],"^.{5}")
+  #Se hace append a un df de resultados
+  missing_municipalities_df<- rbind(missing_municipalities_df,data.frame(as.numeric(tmp)))
+  
+}
+
+#Eliminamos repetidos y cambiamos el nombre de la columna
+missing_municipalities_df <- distinct(missing_municipalities_df) %>% 
+                          rename(id=as.numeric.tmp.)
+
+#Merge del df al de concellos que ya teniamos en base al ID, de modo que tenemos los nombres
+missing_municipalities_df <- merge(municipalities_df,missing_municipalities_df,by='id',all=FALSE)
+
+#Ahora se crea un df para el siguiente bucle....
+missing_municipalities_cut_df <- data.frame()
+
+#... En el cual se va a hacer una petición por cada concello que nos falta, aunque sólo
+#usando los dos primeros caracteres del nombre. Las paradas correspondientes a cada concello
+#las podemos filtrar gracias a los ids
+for(i in 1:nrow(missing_municipalities_df)){
+  #Se extraen los dos primeros cancellos del concello
+  tmp <- str_extract(missing_municipalities_df$municipality[i],"^.{2}")
+  #Se crea un df con los caracteres extraidos y su id correspondiente
+  tmp_df <- data.frame(missing_municipalities_df$id[i],tmp)
+  #Se hace append en un df de resultado
+  missing_municipalities_cut_df<- rbind(missing_municipalities_cut_df,tmp_df)
+  
+}
+#Ahjora se renombran las columnas
+missing_municipalities_cut_df <- missing_municipalities_cut_df %>% 
+                              rename(municipality=tmp) %>%
+                              rename(id=missing_municipalities_df.id.i.)
+
+#Se extrae un df de la columna de los valores unicos de las iniciales de los concellos
+mun_df <- data.frame(municipality=unique(missing_municipalities_cut_df$municipality))
+
+#Se hace peiticion con la que se obtienen las paradas por cada municipio
+#La fucion es diferente a la de un script previo, dado que se va a hacer una peticion
+#para cada par de letras, pero se comprobaran en la misma todos los posibles concellos.
+stops_df <- get_busstops_mun(missing_municipalities_cut) 
+
+#Se guarda el nuevo df de las paradas en un csv
+write.csv(stops_df, "D:\\IFFE\\TFM\\csv\\missing_stops.csv",
+          row.names=FALSE,fileEncoding = "UTF-8")
+
+#Lo volvemos a cargar, con lo que quitamos el problema de location que era lista
+stops_df <- fread("D:\\IFFE\\TFM\\csv\\missing_stops.csv")
+
+#Se  cargan los ids de las paradas que faltaban y de los obtenidos
+tmp_ids <- data.frame(id=as.numeric(missing_stops$id)) 
+
+tmps_ids_got <- data.frame(id=stops_df$id)
+
+#Nos quedamos con las paradas que ha sido imposible retornar
+errors <- anti_join(tmp_ids,tmps_ids_got,by='id') #Son 19
+nrow(errors)
+#Estas paradas hay que eliminarlas del csv de servicios, no queda otra
+
+#Las que tenemos recien conseguidas las añadimos al csv de las paradas
+busstops_df <- fread("busstops.csv")
+
+busstops_df <- bind_rows(busstops_df,stops_df) %>%
+              distinct(id, .keep_all = TRUE)
+
+#Escribimos el nuevo csv de las paradas
+write.csv(busstops_df, "D:\\IFFE\\TFM\\csv\\busstops_comb.csv",
+          row.names=FALSE,fileEncoding = "UTF-8")
+
+#Ahora vamos a filtrar las entradas con paradas erroneas
+services_df <- fread("services_new_id.csv")
+
+#Renombramos las columnas y generamos un dos df, para comparar con los origenes y destinos
+errors_origin <- errors %>% rename(id_origin=id) 
+errors_destination <- errors %>% rename(id_destination=id) 
+
+#Se genera el df con el anti_join en cada columna
+services_df <- services_df %>%
+              anti_join(errors_origin,by="id_origin") %>%
+              anti_join(errors_destination,by="id_destination")
+
+#Guardamos el nuevo csv de los servicios
+write.csv(services_df, "D:\\IFFE\\TFM\\csv\\services_removed_missing.csv",
+          row.names=FALSE,fileEncoding = "UTF-8")
